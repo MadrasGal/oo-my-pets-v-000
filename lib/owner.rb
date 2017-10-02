bf248edfb3501a4e492e3ff45fbac8b4560fe636
Purnima  Lakshmi Narayanan <purnima0912@hotmail.com>
2017-10-02 01:54:42 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,4 +4,40 @@ class Owner
   def self.track(owner)
     @@all +=1
   end
+
+  def species
+  end
+
+  def say_species
+  end
+
+  def name
+  end
+
+  def pets
+  end
+
+  def buy_fish
+  end
+
+  def buy_cat
+  end
+
+  def buy_dog
+  end
+
+  def walk_dogs
+  end
+
+  def play_with_cats
+  end
+
+  def feed_fish
+  end
+
+  def sell_pets
+  end
+
+  def list_pets
+  end
 end
