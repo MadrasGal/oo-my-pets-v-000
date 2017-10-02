ecfb47af8765988225bb3c7943e9124ce0ca0e08
Purnima  Lakshmi Narayanan <purnima0912@hotmail.com>
2017-10-02 02:08:41 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,10 +5,10 @@ attr_accessor :owner_name
 @@all = 0
 track = []
 
-def initialize(owner_name)
-@track << owner_name
-@@all +=1
-end
+  def initialize(owner_name)
+    @track << owner_name
+    @@all +=1
+  end
   def self.all
     @@all
   end
